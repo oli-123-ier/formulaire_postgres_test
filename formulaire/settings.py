9db1f33040ea497bec7c2c295480191631929cda
oli-123-ier <olivierlomie@gmail.com>
2024-05-22 21:02:08 +0000

    allow_host

--- formulaire/settings.py
+++ formulaire/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["formulaire-postgres-test-2.onrender.com"]
 
 
 # Application definition
